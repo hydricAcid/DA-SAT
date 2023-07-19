9c6913c3395eb5f248e99a811768ca9cd5d0e888
hydricAcid <shadowcreeper255@gmail.com>
2023-07-19 10:17:55 +1000

    another update

--- app/index.php
+++ app/index.php
@@ -34,7 +34,7 @@ $dataJson = json_encode($finalData);
             <div class="container">
                 <div class="gauge-a"></div>
                 <div class="gauge-b"></div>
-                <div class="gauge-c"></div>
+                <div class="gauge-c1"></div>
                 <div class="gauge-data">
                   <h1 id="percentgraph1">0%</h1>
                 </div>
@@ -46,7 +46,7 @@ $dataJson = json_encode($finalData);
             <div class="container">
                 <div class="gauge-a"></div>
                 <div class="gauge-b"></div>
-                <div class="gauge-c"></div>
+                <div class="gauge-c2"></div>
                 <div class="gauge-data">
                   <h1 id="percentgraph2">0%</h1>
                 </div>
@@ -66,46 +66,61 @@ $dataJson = json_encode($finalData);
         
         
         <script>
-            window.addEventListener('load', function() {
-            var data = <?php echo $dataJson; ?>;
-            var multiplier = 3; // Define the multiplier value
+        window.addEventListener('load', function() {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var chartContainer = document.getElementById('chartContainer');
+        chartContainer.appendChild(canvas);
 
-            // Multiply the y-values by the multiplier
-            data.forEach(function(point) {
+        // Set the canvas size
+        canvas.width = chartContainer.clientWidth;
+        canvas.height = chartContainer.clientHeight;
+
+        function fetchDataAndRedrawGraph() {
+            // Fetch updated data from the server
+            fetch('test.php')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                var multiplier = 3; // Define the multiplier value
+
+                // Multiply the y-values by the multiplier
+                data.forEach(function(point) {
                 point.y = point.y * multiplier;
-            });
+                });
 
-            var canvas = document.createElement('canvas');
-            var ctx = canvas.getContext('2d');
+                // Clear the canvas before redrawing the graph
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            var chartContainer = document.getElementById('chartContainer');
-            chartContainer.appendChild(canvas);
-
-            // Set the canvas size
-            canvas.width = chartContainer.clientWidth;
-            canvas.height = chartContainer.clientHeight;
-
-            // Draw the line graph
-            ctx.beginPath();
-            ctx.moveTo(0, canvas.height - data[0].y);
-            for (var i = 1; i < data.length; i++) {
+                // Draw the line graph
+                ctx.beginPath();
+                ctx.moveTo(0, canvas.height - data[0].y);
+                for (var i = 1; i < data.length; i++) {
                 var point = data[i];
                 var x = point.x * (canvas.width / (data.length - 1));
                 ctx.lineTo(x, canvas.height - point.y);
-            }
+                }
 
-            // Fill the area under the line graph
-            ctx.lineTo(canvas.width, canvas.height);
-            ctx.lineTo(0, canvas.height);
-            ctx.closePath();
-            ctx.fillStyle = 'rgba(0, 0, 255, 0.2)'; // Set the fill color with transparency
-            ctx.fill();
+                // Fill the area under the line graph
+                ctx.lineTo(canvas.width, canvas.height);
+                ctx.lineTo(0, canvas.height);
+                ctx.closePath();
+                ctx.fillStyle = 'rgba(16, 255, 20, 1)'; // Set the fill color with transparency
+                ctx.fill();
 
-            // Stroke the line graph
-            ctx.strokeStyle = '#000';
-            ctx.stroke();
+                // Stroke the line graph
+                ctx.strokeStyle = '#000';
+                ctx.stroke();
             });
+        }
 
+        // Fetch and redraw the graph initially
+        fetchDataAndRedrawGraph();
+
+        // Update the graph every second
+        setInterval(fetchDataAndRedrawGraph, 1000);
+        });
 
         </script>
     </body>
--- app/test.php
+++ app/test.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<?php
+// Connect to the SQLite database
+$pdo = new PDO('sqlite:/home/kyle/Projects/DA-SAT/Scripts/data1.db');
+
+$query = $pdo->query('SELECT content FROM lines');
+$data = $query->fetchAll(PDO::FETCH_COLUMN, 0);
+
+// Create an array to hold the final data with x and y values
+$finalData = [];
+for ($i = 0; $i < count($data); $i++) {
+    $finalData[] = ['x' => $i + 1, 'y' => $data[$i]];
+}
+
+echo json_encode($finalData);
+?>
