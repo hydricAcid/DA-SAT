f491963c63a0233f5bd91c93417a9598571411dd
hydricAcid <shadowcreeper255@gmail.com>
2023-07-19 15:27:36 +1000

    small update

--- app/cpuFetch.php
+++ app/cpuFetch.php
@@ -1,11 +1,11 @@
 <?php
 // Connect to the SQLite database
-$pdo = new PDO('sqlite:../Scripts/data1.db');
+$pdo = new PDO('sqlite:../Scripts/data1.db'); # relative path: accesses the parent directory then goes into the Scripts folder to access the database
 
 $query = $pdo->query('SELECT content FROM lines');
 $data = $query->fetchAll(PDO::FETCH_COLUMN, 0);
 
-// Create an array to hold the final data with x and y values
+// Create an array to hold the final data with x and y values for the graph
 $finalData = [];
 for ($i = 0; $i < count($data); $i++) {
     $finalData[] = ['x' => $i + 1, 'y' => $data[$i]];
--- app/ramFetch.php
+++ app/ramFetch.php
@@ -1,11 +1,11 @@
 <?php
 // Connect to the SQLite database
-$pdo = new PDO('sqlite:../Scripts/data2.db');
+$pdo = new PDO('sqlite:../Scripts/data2.db'); # relative path: accesses the parent directory then goes into the Scripts folder to access the database
 
 $query = $pdo->query('SELECT percentage FROM ramValues');
 $data = $query->fetchAll(PDO::FETCH_COLUMN, 0);
 
-// Create an array to hold the final data with x and y values
+// Create an array to hold the final data with x and y values for the graph
 $finalData = [];
 for ($i = 0; $i < count($data); $i++) {
     $finalData[] = ['x' => $i + 1, 'y' => $data[$i]];
